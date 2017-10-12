95be7a9385609f7bc404b0b8c7e38c9b9ca8aa79
cesarferreira <cesar.manuel.ferreira@gmail.com>
2017-10-12 12:15:51 +0100

    moved data around

---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+(()=>{const dom={select:document.querySelector.bind(document),slectAll:document.querySelectorAll.bind(document)};const burger=dom.select('.nav-toggle');const menu=dom.select('.nav-menu');burger.addEventListener('click',()=>{burger.classList.toggle('is-active');menu.classList.toggle('is-active');});})();
