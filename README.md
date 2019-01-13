9fc126ae55c5b5b77fc483a138420b2c677a34a8
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2019-01-13 11:57:22 +0000

    added bots

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cesarferreira.github.io
+
+to generate the HTML:
+> node builder.js
--- builder.js
+++ builder.js
@@ -8,23 +8,23 @@ const LAYOUT_FILE_PATH = './templates/layout.hbs';
 const OUTPUT_FILE_PATH = 'index.html';
 
 function readFromFile(fileName) {
-	return fs.readFileSync(fileName, 'utf8')
+    return fs.readFileSync(fileName, 'utf8')
 }
 
 function getObjFromFile(fileName) {
-	return JSON.parse(readFromFile(fileName));
+    return JSON.parse(readFromFile(fileName));
 }
 
 function writeToFile(fileName, content) {
-	fs.writeFileSync(fileName, content, 'utf8');
+    fs.writeFileSync(fileName, content, 'utf8');
 }
 
 function stylizeCategories(data) {
-	data.categories.forEach((category, i) => {
-		category.style = i % 2 === 0 ? 'is-gray' : 'is-bold';
-	});
+    data.categories.forEach((category, i) => {
+        category.style = i % 2 === 0 ? 'is-gray' : 'is-bold';
+    });
 
-	return data;
+    return data;
 }
 
 app.engine('html', require('engine-handlebars'));
@@ -34,31 +34,32 @@ app.create('pages');
 
 // add a template to the collection 
 app.page(OUTPUT_FILE_PATH, {
-	content: readFromFile(LAYOUT_FILE_PATH)
+    content: readFromFile(LAYOUT_FILE_PATH)
 });
 
 let templateData = {
-	categories: [
-		getObjFromFile('data/tools.json'),
-		getObjFromFile('data/blog.json'),
-		getObjFromFile('data/libraries.json'),
-		// getObjFromFile('data/misc.json'),
-		getObjFromFile('data/contacts.json')
-	]
+    categories: [
+        getObjFromFile('data/tools.json'),
+        getObjFromFile('data/blog.json'),
+        getObjFromFile('data/libraries.json'),
+        getObjFromFile('data/bots.json'),
+        // getObjFromFile('data/misc.json'),
+        getObjFromFile('data/contacts.json')
+    ]
 };
 
 templateData = stylizeCategories(templateData);
 // render it 
 app.render(OUTPUT_FILE_PATH, templateData,
-	(err, view) => {
-		if (err !== null) {
-			log(err)
-			process.exit(2)
-		}
-		if (fs.existsSync(OUTPUT_FILE_PATH)) {			
-			fs.unlinkSync(OUTPUT_FILE_PATH);
-		}
-		writeToFile(OUTPUT_FILE_PATH, view.content);
-		log(chalk.green(`File ${chalk.bold.white(OUTPUT_FILE_PATH)} generated...`));
-	}
+    (err, view) => {
+        if (err !== null) {
+            log(err)
+            process.exit(2)
+        }
+        if (fs.existsSync(OUTPUT_FILE_PATH)) {
+            fs.unlinkSync(OUTPUT_FILE_PATH);
+        }
+        writeToFile(OUTPUT_FILE_PATH, view.content);
+        log(chalk.green(`File ${chalk.bold.white(OUTPUT_FILE_PATH)} generated...`));
+    }
 );
